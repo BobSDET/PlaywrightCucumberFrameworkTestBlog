b92d1de53f6f7e22060684c3d9c7a19ad0b13ac5
Babu <yourmail@email.com>
2026-08-04 23:18:39 +0530

    Fixed Allure environment generation

--- src/reporting/Environment.ts
+++ src/reporting/Environment.ts
@@ -1,25 +1,33 @@
 import * as fs from "fs";
 import * as os from "os";
+import * as path from "path";
 import { version as playwrightVersion } from "@playwright/test/package.json";
 
 export class Environment {
 
     static generate(): void {
 
-        const data = `
+        const resultsFolder = path.join(process.cwd(), "allure-results");
 
+        // Create folder if it doesn't exist
+        if (!fs.existsSync(resultsFolder)) {
+            fs.mkdirSync(resultsFolder, { recursive: true });
+        }
+
+        const data = `
 Browser=Chromium
 Environment=QA
 OS=${os.type()} ${os.release()}
 Node=${process.version}
 Playwright=${playwrightVersion}
 BaseURL=https://testautomationpractice.blogspot.com/
-
 `;
 
         fs.writeFileSync(
-            "allure-results/environment.properties",
+            path.join(resultsFolder, "environment.properties"),
             data.trim()
         );
+
+        console.log("Environment.properties created successfully.");
     }
 }
